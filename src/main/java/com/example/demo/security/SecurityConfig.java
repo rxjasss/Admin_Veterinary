e2c6df12760705b0e446e5d24fd62694a3762279
rxjasss <ruben_07@hotmail.es>
2023-04-28 10:46:05 +0200

    Security

--- src/main/java/com/example/demo/security/SecurityConfig.java
+++ src/main/java/com/example/demo/security/SecurityConfig.java
@@ -18,7 +18,7 @@ public class SecurityConfig {
         http.authorizeHttpRequests((requests) -> requests
                 .requestMatchers("/", "/home", "/auth/**", "/about/**", "/error/**", "/webjars/**",
                         "/css/**", "/files/**", "/images/**", "/photos/**")
-                .permitAll().requestMatchers("/users/listUsers","/listUsers","/users/formUser/**","/users/formUser","/deleteUser/**","/activateUser/**","/addUser","/addVeterinary").hasAuthority("ADMIN").anyRequest()
+                .permitAll().requestMatchers("/users/listUsers","/listUsers","/users/formUser/**","/users/formUser","/deleteUser/**","/activateUser/**","/addUser","/addVeterinary","/formPasswordUser/**").hasAuthority("ADMIN").anyRequest()
                 .authenticated())
                 .formLogin((form) -> form.loginPage("/auth/login").defaultSuccessUrl("/users/listUsers").permitAll())
                 .logout((logout) -> logout.permitAll().logoutUrl("/auth/logout")
